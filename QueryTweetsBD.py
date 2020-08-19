9ca55229f40e612e44ddc2e75a19a4a1158f4bde
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2020-08-19 11:40:00 -0300

    Refatorando código

--- QueryTweetsBD.py
+++ QueryTweetsBD.py
@@ -7,28 +7,32 @@ from mysql.connector import Error
 '''Buscar todos os tweets armazenados no banco de dados, e armazenar somente os campos com texto preenchido no tweet
  e o nome do usuário, dentro arquivo de texto'''
 
-try:
-    connection = mysql.connector.connect(host='localhost', user='root', passwd='', database='Twitter')
 
-    query = "select text, name from tweets"
+def buscarTextoUsuario(connection):
     cursor = connection.cursor()
-    cursor.execute(query)
-    tweets = cursor.fetchall()
-    print("Total linhas resultado da consulta: ", cursor.rowcount)
+    try:
+        query = "select text, name from tweets"
+        cursor.execute(query)
+        tweets = cursor.fetchall()
+        print("Total linhas resultado da consulta: ", cursor.rowcount)
 
-    with open('base.txt', 'a') as arquivo:
-        for texto, usuario in tweets:
-            if texto != '':
-                arquivo.write(texto + ',' + usuario)
-                arquivo.write("\n")
+        with open('base.txt', 'a') as arquivo:
+            for texto, usuario in tweets:
+                if texto != '':
+                    arquivo.write(texto + ',' + usuario)
+                    arquivo.write("\n")
 
-except Error as e:
-    print("Erro ao acessar dados da tabela 'tweets' no banco de dados", e)
-finally:
-    if (connection.is_connected()):
-        connection.close()
-        cursor.close()
-        print("conexão com MySQL foi finalizada")
+    except Error as e:
+        print("Erro ao acessar dados da tabela 'tweets' no banco de dados", e)
+    finally:
+        if connection.is_connected():
+            connection.close()
+            cursor.close()
+            print("conexão com MySQL foi finalizada")
+
+    arquivo.close()
 
 
-arquivo.close()
+if __name__ == "__main__":
+    connection = mysql.connector.connect(host='localhost', user='root', passwd='', database='Twitter')
+    buscarTextoUsuario(connection)
